0fa418165dc654be5ab6ecaccea686838e8a9a28
Guinn <eguinjonathan08@gmail.com>
2024-10-03 11:44:45 +0700

    Refactor font imports, remove unused code, and add message form functionality

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,59 +1,92 @@
 "use client";
-import { databases } from '@/libs/appwriteConfig'
-import React, {useState, useEffect} from 'react'
-import { ID } from 'appwrite';
-
+import { databases } from "@/libs/appwriteConfig";
+import React, { useState, useEffect } from "react";
+import { ID, Query } from "appwrite";
 
 const page = () => {
-    const [messages, setMessages] = useState<{ $id: string; body: string; $createdAt: string }[]>([])
-    const [messageBody, setMessageBody] = useState('')
+  const [messages, setMessages] = useState<
+    { $id: string; body: string; $createdAt: string }[]
+  >([]);
+  const [messageBody, setMessageBody] = useState("");
 
-    useEffect(() => {
-        getMessages()
-    }, [])
+  useEffect(() => {
+    getMessages();
+  }, []);
 
-    const getMessages = async () => {
-        const response = await databases.listDocuments(process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!, process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID_MESSAGES!)
-        console.log(response);
-        setMessages(response.documents as unknown as { $id: string; body: string; $createdAt: string }[])
-    }
+  const getMessages = async () => {
+    const response = await databases.listDocuments(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID_MESSAGES!,
+      [Query.orderDesc("$createdAt")]
+    );
+    console.log(response);
+    setMessages(
+      response.documents as unknown as {
+        $id: string;
+        body: string;
+        $createdAt: string;
+      }[]
+    );
+  };
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault()
-        const response = await databases.createDocument(process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!, process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID_MESSAGES!, ID.unique(), {body: messageBody})
-        setMessageBody('')
-        console.log(response);
-        getMessages()
-    }
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await databases.createDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID_MESSAGES!,
+      ID.unique(),
+      { body: messageBody }
+    );
+    setMessageBody("");
+    setMessages((prevState) => [
+      response as unknown as { $id: string; body: string; $createdAt: string },
+      ...prevState,
+    ]);
+  };
 
   return (
-   <main className='container'>
-    <div className='room--container'>
-      <form id='message--form'>
-        <div>
-          <textarea name="" id="" required maxLength={1000} placeholder='say something...' onChange={(e) => {setMessageBody(e.target.value)}} value={messageBody}></textarea>
-        </div>
-        <div className='send-btn--wrapper'>
-          <button type='submit' onClick={handleSubmit} className='bg-red-800 py-2 px-12 rounded-2xl'>Send</button>
-        </div>
-      </form>
-      <div>
-        {messages.map((message) => (
-          <div key={message.$id} className='message--wrapper'>
-
-            <div className='message--header'>
-              <small>{message.$createdAt}</small>
-            </div>
-
-            <div className='message--body'>
-              <span>{message.body}</span>
-            </div>
+    <main className="container">
+      <div className="room--container">
+        <form id="message--form">
+          <div>
+            <textarea
+              name=""
+              id=""
+              required
+              maxLength={1000}
+              placeholder="say something..."
+              onChange={(e) => {
+                setMessageBody(e.target.value);
+              }}
+              value={messageBody}
+            ></textarea>
           </div>
-        ))}
-      </div>
-    </div>
-   </main>
-  )
-}
+          <div className="send-btn--wrapper">
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="bg-red-800 py-2 px-12 rounded-2xl"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+        <div>
+          {messages.map((message) => (
+            <div key={message.$id} className="message--wrapper">
+              <div className="message--header">
+                <small>{message.$createdAt}</small>
+              </div>
 
-export default page
+              <div className="message--body">
+                <span>{message.body}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default page;
--- libs/appwriteConfig.ts
+++ libs/appwriteConfig.ts
@@ -1,9 +1,5 @@
 import { Client, Databases } from "appwrite";
 
-export const PROJECT_ID = '66fd38030033260b0fb0';
-export const DATABASE_ID = '66fd3aad0013ca246677';
-export const COLLECTION_ID_MESSAGES = '66fd3ab8002bb98b7b97';
-
 const client = new Client();
 
 client
